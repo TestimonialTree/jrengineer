42ac086df20b641286f613e20b296fd36a44c488
jermahs <james@testimonialtree.com>
2022-02-22 14:04:01 -0500

    cleanup

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import './App.css'
 import IncrementingButton from './problems/IncrementingButton';
 import WorkingWithArrays from './problems/WorkingWithArrays';
-import Question from './ProblemWrapper';
+import ProblemWrapper from './ProblemWrapper';
 
 // Ground Rules:
 // Anything goes... you can google it, you can ask questions, you can do basically anything except "Phone a Friend". 
@@ -11,23 +11,27 @@ import Question from './ProblemWrapper';
 // The worst thing you can do is: nothing. If you just sit there and stare, then there's no chance. If you struggle through something and talk through it, then
 // I'm glad to facilitate. 
 
+// Prerequisites:
+// - npm install
+// - npm run dev
+
 // TODO - first create a branch /interviews/{lastname-firstname}
 // 1. Add a background color on the entire page to be #282c34 (text white)
 // 2. Add a block of larger text on the page with your name
 // 3. Add some spacing around the entire page
 // 4. Add some padding or margin around each question 
-// 5. Make the description of the question bold and of size 12px
+// 5. Make the description of the question bold and a little larger
 
 function App() {
   return (
     <div>
-      <Question description="Implement incrementing button.">
+      <ProblemWrapper description="Implement incrementing button.">
         <IncrementingButton />
-      </Question>
+      </ProblemWrapper>
 
-      <Question description="Working with arrays">
+      <ProblemWrapper description="Working with arrays">
         <WorkingWithArrays input={[1, 2, 3, 4, 5]} />
-      </Question>
+      </ProblemWrapper>
     </div>
   )
 }
--- src/problems/IncrementingButton.jsx
+++ src/problems/IncrementingButton.jsx
@@ -1,6 +1,7 @@
 import { useState } from 'react';
 
 // 1. implement handleClick to increment the count by 1 on each click
+// 2. add a button to reset the count back to zero
 
 export default function IncrementingButton() {
 
